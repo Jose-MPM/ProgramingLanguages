b65cce782fb459a5e223087b30e6791327098547
osrm17 <rg.oscar17@ciencias.unam.mx>
2022-05-26 07:33:12 -0500

    Función corrupta y más cosas.

--- Practica03/Practica03.hs
+++ Practica03/Practica03.hs
@@ -80,12 +80,13 @@ data Expr
   | Let Expr Expr
   | Fn Identifier Expr -- funciones anonimas  
   | App Expr Expr -- e1 e2
+
   | L Int -- direccion de memoria 
   | Alloc Expr -- crear una celda de memoria y obtener su direccion
   | Dref Expr -- obtener el valor almacenado en una direccion de memoria
   | Assign Expr Expr -- almacenar un valor en una celda de memoria
   | Void
-  | Sew Expr Expr
+  | Seq Expr Expr
   | While Expr Expr
   deriving (Eq)
 
@@ -96,11 +97,18 @@ instance Show Expr where
   show (Un op e) = show op ++ show e
   show (Bin op e1 e2) = "(" ++ show e1 ++ " " ++ show op ++ " " ++ show e2 ++ ")"
   show (If e1 e2 e3) = "if " ++ show e1 ++ " then " ++ show e2 ++ " else " ++ show e3
---  show (Let e1 e2) = 
+  show (Let e1 (Fn idr e2)) = "let " ++ show idr ++ " = " ++ show e1 ++ " in " ++ show e2
   show (Fn idr e) = "λ" ++ idr ++ "." ++ show e
-  show (App e1 e2) = show e1 ++ " " ++ show e2 
+  show (App e1 e2) = show e1 ++ " " ++ show e2
+  show (L dir) = "l " ++ show dir
+  show (Alloc e) = "ref " ++ show e
+  show (Dref e) = "! " ++ show e
+  show (Assign e1 e2) = show e1 ++ " := " ++ show e2
+  show (Void) = "()"
+  show (Seq e1 e2) = show e1 ++ "; " ++ show e2
   show e = error "implementar"
 
+--Let (I 10) (Fn ("x") (Bin Eq (I 0) (Un Succ (V "x"))))
 
 type Address = Int
 type Value = Expr
@@ -113,17 +121,32 @@ newAddress _ = error "implementar"
 access :: Address -> Memory -> Maybe Value
 access _ = error "implementar"
 
+-- Oscar
 update :: Cell -> Memory -> Maybe Memory
-update _ = error "implementar"
+update cell memory = error "implementar"
+
+-- | Indica si la memoria es corrupta
+
+-- Ejemplos
+-- >>> corrupted $ [(1, I 2), (1, I 3)]
+-- >>> corrupted $ [(1, I 2), (2, I 3), (3, V "x")]
+corrupted :: Memory -> Bool
+corrupted ((dir, value):xs) = any (\x -> (fst x) == dir) xs
+
 
 -- | Obtine el conjunto de variable de una expresión
 
+-- Oscar 
 -- Ejemplos
 -- >>> frVars $ Bin Add (I 4) (V "x")
 frVars :: Expr -> [Identifier]
 frVars (V idf) = [idf]
 frVars (Un op e) = frVars e
 frVars (Bin op e1 e2) = frVars e1 ++ frVars e2
+frVars (If e1 e2 e3) = frVars e1 ++ frVars e2 ++ frVars e3 
+frVars (Let e1 e2) = frVars e1 ++ frVars e2
+frVars (Fn x e) = filter (/= x) (frVars e)
+frVars (App e1 e2) = frVars e1 ++ frVars e2
 frVars _ = []
 
 type Substitution = (Identifier, Expr)
@@ -135,6 +158,7 @@ subst _ _ = error "implementar"
 eval1 :: (Memory, Expr) -> (Memory, Expr)
 eval1 _ = error "implementar"
 
+-- Oscar
 evals :: (Memory, Expr) -> (Memory, Expr)
 evals _ = error "implementar"
 
