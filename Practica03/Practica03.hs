dffc8c6ad9b76fd70b45f6704b9a553fabbbb9da
Ale <alejaortegarcia@ciencias.unam.mx>
2022-05-28 20:07:49 -0500

    access modificao, eval1 terminao(?

--- Practica03/Practica03.hs
+++ Practica03/Practica03.hs
@@ -1,6 +1,7 @@
 module MiniC where
 
 import Data.List
+import Data.Maybe
 
 {- | Practica 03
      
@@ -83,7 +84,7 @@ data Expr
   | Fn Identifier Expr -- funciones anonimas  
   | App Expr Expr -- e1 e2
 
-  | L Int -- direccion de memoria 
+  | L {get :: Int}-- direccion de memoria 
   | Alloc Expr -- crear una celda de memoria y obtener su direccion
   | Dref Expr -- obtener el valor almacenado en una direccion de memoria
   | Assign Expr Expr -- almacenar un valor en una celda de memoria
@@ -108,6 +109,7 @@ instance Show Expr where
   show (Assign e1 e2) = show e1 ++ " := " ++ show e2
   show (Void) = "()"
   show (Seq e1 e2) = show e1 ++ "; " ++ show e2
+  show (While e1 e2) = "While (" ++ show e1 ++ ", " ++show e2 ++ ")"
   show e = error "implementar"
 
 --Let (I 10) (Fn ("x") (Bin Eq (I 0) (Un Succ (V "x"))))
@@ -126,12 +128,12 @@ type Memory = [Cell]
 -- >>> newAddress [( 0, I 21), (1, Void), (2, I 12)]
 -- >>> newAddress [(0, I 21), (1, Void), (2, I 12), (1, B True)]
 newAddress :: Memory -> Expr
-newAddress xs 
-  | corrupted xs = error "Memory corrupted."
-  | otherwise = L $ (minFree . fst . unzip) xs 
+newAddress ms 
+  | corrupted ms = error "Memory corrupted."
+  | otherwise = L $ (minFree . fst . unzip) ms 
 
 minFree :: [Int] -> Int
-minFree xs = minFrom 0 (length xs,xs)
+minFree xs = minFrom 0 (length xs, xs)
 
 minFrom :: Int -> (Int,[Int]) -> Int
 minFrom a (n,xs)
@@ -142,20 +144,18 @@ minFrom a (n,xs)
        b = a + 1 + div n 2 
        m = length us
 
--- access 1 [] 
--- >>> Nothing
--- access 0 [(1, 1 9), (2, I 22)]
--- >>> Nothing
--- access access 2 [(0, B False), (2, I 90)]
--- >>> Just 90
+-- | Dada una dirección de memoria, devuelve el valor contenido en la celda con tal
+--   dirección.
+
+-- Ejemplos
+-- >>> access 1 [] 
+-- >>> access 0 [(1, 1 9), (2, I 22)]
+-- >>> access 2 [(0, B False), (2, I 90)]
 access :: Address -> Memory -> Maybe Value
 access _ [] = Nothing
-access n m@((a,v):xs) = if nub [ n | (a,b) <- m,n <- [a]] == [ n | (a,b) <- m,n <- [a]] -- Verificamos que no tengamos direcciones repetidas
-                        then 
-                          if a == n
-                            then Just v
-                            else access n xs
-                        else error "Memory corrupted" 
+access n m@((a, v):ms) 
+  | corrupted m = error "Memory corrupted."
+  | otherwise = lookup n m
 
 -- | Dada una celda y una memoria tal vez actualiza la memoria
 -- con la nueva celda.
@@ -203,12 +203,17 @@ frVars _ = []
 type Substitution = (Identifier, Expr)
 
 
--- b= Bin Add (V "x") (I 34)
--- subs b ("x", I 2)
+-- | Dada un expresion y una sustitución, regresa la
+--   expresion con la sustitucion.
+
+-- Ejemplos
+-- >>> subst (Add (V ”x”) (I 5)) (”x”,I 10)
+-- >>> subst (Let ”x” (I 1) (V ”x”)) (”y”, Add (V ”x”) (I 5))
 subst :: Expr -> Substitution -> Expr
-subst (V idf) (x, e1) = if x == idf
-                        then e1
-                        else (V idf)
+subst (V idf) (x, e1) 
+  | x == idf = e1
+  | otherwise = (V idf)
+
 subst (I n) _ = I n
 subst (B b) _ = B b
 subst (Un op e) s = Un op (subst e s)
@@ -218,6 +223,7 @@ subst (Let e1 e2) s = Let (subst e1 s) (subst e2 s)
 subst (Fn x e) s@(y, z)
   | x == y || elem x (frVars z) = error "Invalid substitution"
   | otherwise = Fn x (subst e s)
+
 subst (App e1 e2) s = App (subst e1 s) (subst e2 s)
 subst (L i) _ = (L i) -- No se puede ahcer una substitucion en una memoria 
 subst (Alloc e1) s = Alloc (subst e1 s)
@@ -231,12 +237,81 @@ subst (While e1 e2) s = While (subst e1 s) (subst e2 s)
 --   la reducción a un paso.
 
 -- Ejemplos
--- >>> eval1 ([(0, B False)], (Add (I 1) (I 2)))
--- >>> eval1 ([(0, B False)], (Let ”x” (I 1) (Add (V ”x”) (I 2))))
--- >>> eval1 ([(0, B False)], Assig (L 0 ) (B True))
--- >>> eval1 ([], While (B True) (Add (I 1) (I 1)))
+-- >>> eval1 ([(0, B False)], (Bin Add (I 1) (I 2)))
+-- >>> eval1 ([(0, B False)], (Let (I 1) (Fn ("x") (Bin Add (V "x") (I 2)))))
+-- >>> eval1 ([(0, B False)], (Assign (L 0 ) (B True)))
+-- >>> eval1 ([], While (B True) (Bin Add (I 1) (I 1)))
+-- >>> eval1 ([(2, B True)], Alloc (I 3))
 eval1 :: (Memory, Expr) -> (Memory, Expr)
-eval1 _ = error "implementar"
+eval1 e@(xs, (I n)) = e
+eval1 e@(xs, (B b)) = e
+eval1 (xs, (Un op e)) = 
+  case (op, e) of
+    (Succ, (I n)) -> (xs, I (n + 1))
+    (Pred, (I n)) -> (xs, I (n - 1))
+    (IsZero, (I n)) -> (xs, B (n == 0))
+    (Not, (B b)) -> (xs, B (not b))
+    (_,_) -> let (ms, e') = (eval1 (xs, e)) in (ms, Un op e')
+
+eval1 (xs, Bin op e1 e2) = 
+  case (op, e1, e2) of
+    (Add, I n, I m) -> (xs, I (m + n))
+    (Mul, I n, I m) -> (xs, I (m*n))
+    (Lt, I n, I m) -> (xs, B (n < m))
+    (Gt, I n, I m) -> (xs, B (n > m))
+    (Eq, I n, I m) -> (xs, B (n == m))
+    (And, B b1, B b2) -> (xs, B (b1 && b2))
+    (Or, B b1, B b2) -> (xs, B (b1 || b2))
+    (_, I n, e) -> let (ms, e') = (eval1 (xs, e)) in (ms, Bin op (I n) e')
+    (_, B b, e) -> let (ms, e') = (eval1 (xs, e)) in (ms, Bin op (B b) e')
+    (_,_,_) -> let (ms, e') = (eval1 (xs, e1)) in (ms, Bin op e' e2)
+
+eval1 (xs, If e1 e2 e3) = 
+  case e1 of
+    (B (True)) -> (xs, e2)
+    (B (False)) -> (xs, e3)
+    (e) -> let (ms, e') = (eval1 (xs, e)) in (ms, If e' e2 e3)
+
+eval1 (xs, Let e1 all@(Fn x e2)) = 
+  case e1 of
+    (I n) -> (xs, subst e2 (x, e1))
+    (B b) -> (xs, subst e2 (x, e1))
+    (e) -> let (ms, e') = (eval1 (xs, e)) in (ms, Let e' all)
+
+eval1 (xs, App e1 e2) = 
+  case (e1, e2) of
+    (I n, e2) -> let (ms, e) = (eval1 (xs, e2)) in (ms, App e1 e)
+    (B b, e2) -> let (ms, e) = (eval1 (xs, e2)) in (ms, App e1 e)
+    (Fn x e, I n) -> (xs, subst e1 (x, e2))
+    (Fn x e, B b) -> (xs, subst e1 (x, e2)) 
+    (_,_) -> let (ms, e) = (eval1 (xs, e1)) in (ms, App e e2)
+
+eval1 (xs, Alloc e) = 
+  case e of 
+    (I n) -> let l = newAddress xs in ((get l, e):xs, l)
+    (B n) -> let l = newAddress xs in ((get l, e):xs, l)
+    (e) -> let (ms, e') = (eval1 (xs, e)) in (ms, Alloc e')
+
+eval1 (xs, Dref e) =
+  case e of 
+    (L n) -> let v = access n xs in (xs, fromJust v)
+    (e) -> let (ms, e') = (eval1 (xs, e)) in (ms, Dref e')
+
+eval1 (xs, Assign e1 e2) =
+  case (e1, e2) of
+    (L n, I m) -> let ms = fromJust $ update (n, e2) xs in (ms, Void)
+    (L n, B b) -> let ms = fromJust $ update (n, e2) xs in (ms, Void)
+    (L n, e) -> let (ms, e') = (eval1 (xs, e2)) in (ms, Assign e1 e')
+    (_,_) -> let (ms, e') = (eval1 (xs, e1)) in (ms, Assign e' e2)
+
+eval1 (xs, Seq e1 e2) = 
+  case (e1, e2) of
+    (Void, e) -> (xs, e)
+    (_,_) -> let (ms, e') = (eval1 (xs, e1)) in (ms, Seq e' e2)
+
+eval1 (xs, all@(While e1 e2)) = (xs, If e1 (Seq e2 all) Void)
+eval1 e'@(xs, e) = e'
+
 
 -- Oscar
 evals :: (Memory, Expr) -> (Memory, Expr)
